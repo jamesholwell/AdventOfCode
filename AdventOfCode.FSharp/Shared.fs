da11715df972bd92a5bb4f1b3de1792def6049d6
James Holwell <james@abstractleap.com>
2022-12-01 06:43:14 +0000

    updated shared string functions

--- AdventOfCode.FSharp/Shared.fs
+++ AdventOfCode.FSharp/Shared.fs
@@ -2,9 +2,13 @@
 
 open System
 
+let NormalizeLineEndings (s: string) =
+    s.Replace("\r\n", "\n")
+
 let Split (s: string) =
-    s
-        .Replace("\r\n", "\n")
-        .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    NormalizeLineEndings(s).Split("\n", StringSplitOptions.RemoveEmptyEntries)
+
+let SplitBy (b: string) (s: string) =
+    NormalizeLineEndings(s).Split(b, StringSplitOptions.RemoveEmptyEntries)
 
 let SplitInt = Split >> Seq.map int
