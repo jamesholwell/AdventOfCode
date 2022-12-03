a6e2c32897f16123206dc5e9e336d5c886998160
James Holwell <james@abstractleap.com>
2022-12-03 08:02:28 +0000

    day 3 part 2

--- AdventOfCode.FSharp/2022/Day3.fs
+++ AdventOfCode.FSharp/2022/Day3.fs
@@ -2,6 +2,8 @@
 
 open AdventOfCode.Core
 open AdventOfCode.FSharp
+open System
+open System.Linq
 open Xunit
 
 module Day3 =
@@ -11,6 +13,13 @@ module Day3 =
         Shared.Split 
         >> Seq.map (fun s -> Seq.find (fun (ss : char) -> s[(s.Length / 2)..(s.Length)].Contains(ss)) s[0..(s.Length / 2)])
         >> Seq.map (fun c -> priorities[c])
+        >> Seq.sum
+
+    let SolvePartTwo = 
+        Shared.Split 
+        >> Seq.chunkBySize 3
+        >> Seq.map (Seq.reduce (fun a b -> String.Concat(a.Intersect(b))))
+        >> Seq.map (fun c -> priorities[Seq.head c])
         >> Seq.sum
 
     let exampleInput =
@@ -25,9 +34,14 @@ CrZsJsPPZsGzwwsLwLmpwMDw"
     let ``Solves Part One Example`` () =
         let actual = Solve exampleInput
         Assert.Equal(157, actual)
+
+    [<Fact>]
+    let ``Solves Part Two Example`` () =
+        let actual = SolvePartTwo exampleInput
+        Assert.Equal(70, actual)
         
 
 type Day3(input: string) =
     interface ISolver with
         member this.SolvePartOne() = Day3.Solve input |> string
-        member this.SolvePartTwo() = failwith "Solve part 1 first"
+        member this.SolvePartTwo() = Day3.SolvePartTwo input |> string
