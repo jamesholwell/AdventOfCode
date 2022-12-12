0d61c224d7028e08485f232afc8f0c59ac9f3c03
James Holwell <james@abstractleap.com>
2022-12-12 07:59:35 +0000

    day 12 (but it doesnt work)

---
+++ AdventOfCode.CSharp/2022/Day12.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.CSharp._2022;
+
+public class Day12 : Solver {
+    private readonly ITestOutputHelper? io;
+
+    private Dictionary<Tuple<int, int>, int> unvisited;
+
+    private Dictionary<Tuple<int, int>, int> tentative;
+
+    private Dictionary<Tuple<int, int>, int> elevations;
+
+    //public Day12(string? input = null, ITestOutputHelper? io = null) : base(input) {
+    //    this.io = io;
+    //}
+
+    public Day12(string? input = null) : base(input) { }
+
+    public override long SolvePartOne() {
+        var lines = Shared.Split(Input);
+
+        elevations = lines.SelectMany((l, y) => l.Trim().ToCharArray().Select((c, x) =>
+            new KeyValuePair<Tuple<int, int>, int>(Tuple.Create(x, y),
+                c == 'S' ? -1 : c == 'E' ? 1 + 'z' - 'a' : (int) c - (int) 'a'))).ToDictionary(p => p.Key, p => p.Value);
+
+        var height = lines.Length;
+        var width = lines[0].Length;
+
+        unvisited = elevations.ToDictionary(p => p.Key, p => int.MaxValue);
+        tentative = elevations.ToDictionary(p => p.Key, p => int.MaxValue);
+        unvisited[elevations.Single(e => e.Value == -1).Key] = 0;
+        tentative[elevations.Single(e => e.Value == -1).Key] = 0;
+
+        while (unvisited.Any()) {
+            var current = unvisited.MinBy(p => p.Value).Key;
+
+            // up
+            if (current.Item2 > 0) {
+                Visit(current, Tuple.Create(current.Item1, current.Item2 - 1));
+            }
+
+            // down
+            if (current.Item2 < height - 1) {
+                Visit(current, Tuple.Create(current.Item1, current.Item2 + 1));
+            }
+
+            // left
+            if (current.Item1 > 0) {
+                Visit(current, Tuple.Create(current.Item1 - 1, current.Item2));
+            }
+
+            // down
+            if (current.Item1 < width - 1) {
+                Visit(current, Tuple.Create(current.Item1 + 1, current.Item2));
+            }
+
+            unvisited.Remove(current);
+        }
+
+        return tentative[elevations.Single(e => e.Value == 1 + 'z' - 'a').Key]        ;
+    }
+
+    private void Visit(Tuple<int, int> current, Tuple<int, int> prospect) {
+        var isConnected = elevations[prospect] - elevations[current] < 2;
+        var isUnvisited = unvisited.ContainsKey(prospect);
+
+        if (isConnected && isUnvisited && tentative[current] < tentative[prospect]) {
+            unvisited[prospect] = tentative[current] + 1;
+            tentative[prospect] = tentative[current] + 1;
+        }
+    }
+
+    private const string? ExampleInput = @"
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+";
+
+    [Fact]
+    public void SolvesPartOneExample() {
+        var actual = new Day12(ExampleInput).SolvePartOne();
+        Assert.Equal(31, actual);
+    }
+}
