c2e01ad392dfd447221b7742c102ff9f7c3009ff
James Holwell <james@abstractleap.com>
2022-12-18 22:42:11 +0000

    day 18 part two

--- AdventOfCode.CSharp/2022/Day18.cs
+++ AdventOfCode.CSharp/2022/Day18.cs
@@ -1,9 +1,17 @@
-﻿using AdventOfCode.Core;
+﻿using System.Runtime.CompilerServices;
+using AdventOfCode.Core;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AdventOfCode.CSharp._2022;
 
 public class Day18 : Solver {
+    private ITestOutputHelper io;
+
+    //public Day18(ITestOutputHelper io, string? input = null) : base(input) {
+    //    this.io = io;
+    //}
+
     public Day18(string? input = null) : base(input) { }
 
     public override long SolvePartOne() {
@@ -11,6 +19,60 @@ public class Day18 : Solver {
         var faces = Faces(origins);
         
         return faces.GroupBy(f => f).Count(g => g.Count() == 1);
+    }
+
+    public override long SolvePartTwo() {
+        var origins = Parse().ToArray();
+
+        var maxX = origins.Max(o => o.x);
+        var maxY = origins.Max(o => o.y);
+        var maxZ = origins.Max(o => o.z);
+        var filled = new bool[maxX + 1, maxY + 1, maxZ + 1];
+
+        foreach (var origin in origins)
+            filled[origin.x, origin.y, origin.z] = true;
+
+        var seen = new HashSet<(int x, int y, int z)>();
+        var fills = new Queue<(int x, int y, int z)>();
+        fills.Enqueue((0, 0, 0));
+
+        Console.WriteLine($"Search space is {maxX} * {maxY} * {maxZ} = {maxX * maxY * maxZ}");
+
+        while (fills.TryDequeue(out var c)) {
+            if (seen.Contains(c)) continue;
+            
+            seen.Add(c);
+            filled[c.x, c.y, c.z] = true;
+
+            // towards
+            if (c.z > 0 && !filled[c.x, c.y, c.z - 1]) fills.Enqueue((c.x, c.y, c.z - 1));
+
+            // right
+            if (c.x < maxX && !filled[c.x + 1, c.y, c.z]) fills.Enqueue((c.x + 1, c.y, c.z));
+
+            // up
+            if (c.y < maxY && !filled[c.x, c.y + 1, c.z]) fills.Enqueue((c.x, c.y + 1, c.z));
+
+            // away 
+            if (c.z < maxZ && !filled[c.x, c.y, c.z + 1]) fills.Enqueue((c.x, c.y, c.z + 1));
+
+            // left
+            if (c.x > 0 && !filled[c.x - 1, c.y, c.z]) fills.Enqueue((c.x - 1, c.y, c.z));
+
+            // down
+            if (c.y > 0 && !filled[c.x, c.y - 1, c.z]) fills.Enqueue((c.x, c.y - 1, c.z));
+        }
+
+        var allCoordinates = Enumerable.Range(0, maxX)
+            .SelectMany(x => Enumerable.Range(0, maxY).Select(y => (x, y)))
+            .SelectMany(p => Enumerable.Range(0, maxZ).Select(z => (x: p.Item1, y: p.Item2, z)))
+            .ToArray();
+
+        var internalCubes = allCoordinates.Where(p => !filled[p.x, p.y, p.z]);
+
+        var exteriorFaces = Faces(origins.Concat(internalCubes));
+
+        return exteriorFaces.GroupBy(f => f).Count(g => g.Count() == 1);
     }
 
     private IEnumerable<(int x, int y, int z)> Parse() {
@@ -30,7 +92,7 @@ public class Day18 : Solver {
         Up
     }
 
-    private IEnumerable<(int, int, int, Face)> Faces(IEnumerable<(int, int, int)> positions) {
+    private IEnumerable<(int x, int y, int z, Face f)> Faces(IEnumerable<(int, int, int)> positions) {
         /*
          * Decompose the cube into faces, there are three characteristic planes from the origin
          *
@@ -63,8 +125,6 @@ public class Day18 : Solver {
         }
     }
 
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
-
     private const string? ExampleInput = @"
 2,2,2
 1,2,2
@@ -85,5 +145,11 @@ public class Day18 : Solver {
     public void SolvesPartOneExample() {
         var actual = new Day18(ExampleInput).SolvePartOne();
         Assert.Equal(64, actual);
+    }
+
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = new Day18(ExampleInput).SolvePartTwo();
+        Assert.Equal(58, actual);
     }
 }
