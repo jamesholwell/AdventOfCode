0df8c2081365bebe276af17902da9a1a90d12b31
James Holwell <james@abstractleap.com>
2022-12-19 21:24:42 +0000

    (framework) powershell helper

---
+++ AdventOfCode/init.ps1
@@ -0,0 +1,10 @@
+﻿function global:aoc {
+    Param([parameter(ValueFromRemainingArguments = $true)][string[]]$args)
+    dotnet .\bin\Release\net6.0\AdventOfCode.dll @args
+}
+
+function global:rebuild {
+    dotnet build -c Release
+}
+
+rebuild
