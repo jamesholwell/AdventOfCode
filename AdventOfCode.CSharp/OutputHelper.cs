bdc028adc05a83da6fe4a2e0d1ff4ee39209f50c
James Holwell <james@abstractleap.com>
2022-12-20 10:47:26 +0000

    (framework) add tracing output helper

--- AdventOfCode.CSharp/OutputHelper.cs
+++ AdventOfCode.CSharp/OutputHelper.cs
@@ -8,6 +8,12 @@ public class ConsoleOutputHelper : ITestOutputHelper {
     public void WriteLine(string format, params object[] args) => Console.WriteLine(format, args);
 }
 
+public class NullOutputHelper : ITestOutputHelper {
+    public void WriteLine(string value) { return; }
+
+    public void WriteLine(string format, params object[] args) { return; }
+}
+
 public static class OutputHelperExtensions {
     public static void WriteLine(this ITestOutputHelper outputHelper) => outputHelper.WriteLine(string.Empty);
 
