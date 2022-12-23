a4de150827b1ab58e35213fed7a6b3770931d810
James Holwell <james@abstractleap.com>
2022-12-23 18:37:37 +0000

    2022 day 23 part one

--- AdventOfCode.CSharp/2022/Day23.cs
+++ AdventOfCode.CSharp/2022/Day23.cs
@@ -8,10 +8,61 @@ public class Day23 : Solver {
     public Day23(string? input = null, ITestOutputHelper? outputHelper = null) : base(input, outputHelper) { }
 
     private class Elf {
+        // x runs left to right; y runs from top to bottom
         public (int x, int y) Position;
 
         public Elf(int row, int column) {
             this.Position = (column, row);
+        }
+
+        public (Elf, (int x, int y))? ProposedNextPosition(HashSet<(int x, int y)> currentPositions, int round) {
+            var x = Position.x;
+            var y = Position.y;
+
+            // grid from top left
+            var isEmptyNorthWest = !currentPositions.Contains((x - 1, y - 1));
+            var isEmptyNorth     = !currentPositions.Contains((x    , y - 1));
+            var isEmptyNorthEast = !currentPositions.Contains((x + 1, y - 1));
+            
+            var isEmptyWest      = !currentPositions.Contains((x - 1, y    ));
+            var isEmptyEast      = !currentPositions.Contains((x + 1, y    ));
+
+            var isEmptySouthWest = !currentPositions.Contains((x - 1, y + 1));
+            var isEmptySouth     = !currentPositions.Contains((x    , y + 1));
+            var isEmptySouthEast = !currentPositions.Contains((x + 1, y + 1));
+
+            // compass from north west
+            var isEmptyNortherly = isEmptyNorthWest && isEmptyNorth && isEmptyNorthEast;
+            var isEmptyEasterly = isEmptyNorthEast && isEmptyEast && isEmptySouthEast;
+            var isEmptySoutherly = isEmptySouthEast && isEmptySouth && isEmptySouthWest;
+            var isEmptyWesterly = isEmptySouthWest && isEmptyWest && isEmptyNorthWest;
+
+            if (isEmptyNortherly && isEmptyEasterly && isEmptySoutherly && isEmptyWesterly)
+                return null;
+
+            for (var look = 0; look < 4; ++look) {
+                var looking = (look + round) % 4;
+
+                switch (looking) {
+                    case 0:
+                        if (isEmptyNortherly) return (this, (x, y - 1));
+                        break;
+
+                    case 1:
+                        if (isEmptySoutherly) return (this, (x, y + 1));
+                        break;
+
+                    case 2:
+                        if (isEmptyWesterly) return (this, (x - 1, y));
+                        break;
+
+                    case 3:
+                        if (isEmptyEasterly) return (this, (x + 1, y));
+                        break;
+                }
+            }
+
+            return null;
         }
     }
 
@@ -22,16 +73,28 @@ public class Day23 : Solver {
             .Select(probablyElf => probablyElf!)
             .ToArray();
 
-    private string Render(Elf[] elves, out int emptySpaces) {
+    private string Render(IEnumerable<Elf> elves, out int emptySpaces) {
         var positions = new HashSet<(int x, int y)>(elves.Select(e => e.Position));
 
         var minX = positions.Min(t => t.x);
         var maxX = positions.Max(t => t.x);
         var minY = positions.Min(t => t.y);
         var maxY = positions.Max(t => t.y);
-        var buffer = new StringBuilder((maxX - minX + 2) * (maxY - minY + 1));
+
+        return RenderInner(positions, minX, maxX, minY, maxY, out emptySpaces);
+    }
+
+    private string Render(IEnumerable<Elf> elves, int minX, int maxX, int minY, int maxY) {
+        var positions = new HashSet<(int x, int y)>(elves.Select(e => e.Position));
+        return RenderInner(positions, minX, maxX, minY, maxY, out _);
+    }
+
+    private string RenderInner(HashSet<(int x, int y)> positions, int minX, int maxX, int minY, int maxY, out int emptySpaces) {
+        var buffer = new StringBuilder((maxX - minX + 2) * (maxY - minY + 1) + 50);
         emptySpaces = 0;
-        
+
+        buffer.Append(Environment.NewLine);
+
         for (var y = minY; y <= maxY; y++) {
             for (var x = minX; x <= maxX; x++) {
                 var elfIsHere = positions.Contains((x, y));
@@ -45,9 +108,49 @@ public class Day23 : Solver {
         return buffer.ToString();
     }
 
-    public override long SolvePartOne() => 0;
+    private static void ExecuteRound(Elf[] elves, int round) {
+        var currentPositions = new HashSet<(int x, int y)>(elves.Select(e => e.Position));
 
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
+        var movements = 
+            elves
+            .AsParallel()
+            .Select(e => e.ProposedNextPosition(currentPositions, round))
+            .Where(p => p != null)
+            .GroupBy(p => p!.Value.Item2)
+            .Where(g => g.Count() == 1)
+            .Select(g => g.Single()!.Value);
+
+        foreach (var movement in movements) {
+            movement.Item1.Position = movement.Item2;
+        }
+    }
+
+    public override long SolvePartOne() {
+        var elves = Parse(Input);
+        var emptySpaces = 0;
+
+        Trace.WriteLine(Render(elves, out _));
+
+        // tick!
+        for (var i = 0; i < 10; ++i) {
+            ExecuteRound(elves, i);
+
+            Trace.WriteLine();
+            Trace.WriteLine(Render(elves, out emptySpaces));
+            Trace.WriteLine();
+        }
+
+        return emptySpaces;
+    }
+    
+    private const string SmallExampleInput = @"
+.....
+..##.
+..#..
+.....
+..##.
+.....
+";
 
     private const string ExampleInput = @"
 ....#..
@@ -61,10 +164,85 @@ public class Day23 : Solver {
 
     [Fact] public void ParsesInputCorrectly() {
         var elves = Parse(ExampleInput);
-        var actual = Render(elves, out _);
+        var actual = Render(elves, 0, 6, 0, 6);
         Output.WriteLine(actual);
 
-        Assert.Equal(ExampleInput.ReplaceLineEndings(), Environment.NewLine + actual);
+        Assert.Equal(ExampleInput.ReplaceLineEndings(), actual);
+    }
+
+    [Fact]
+    public void ModelsPartOneSmallExampleCorrectly() {
+        var elves = Parse(SmallExampleInput);
+
+        const string expectedRound0 = @"
+.....
+..##.
+..#..
+.....
+..##.
+.....
+";
+
+        var actualRound0 = Render(elves, 0, 4, 0, 5);
+        Trace.WriteLine(actualRound0);
+        Trace.WriteLine();
+        Assert.Equal(expectedRound0.ReplaceLineEndings(), actualRound0);
+
+        ExecuteRound(elves, 0);
+
+        const string expectedRound1 = @"
+..##.
+.....
+..#..
+...#.
+..#..
+.....
+";
+
+        var actualRound1 = Render(elves, 0, 4, 0, 5);
+        Trace.WriteLine(actualRound1);
+        Trace.WriteLine();
+        Assert.Equal(expectedRound1.ReplaceLineEndings(), actualRound1);
+
+        ExecuteRound(elves, 1);
+
+        const string expectedRound2 = @"
+.....
+..##.
+.#...
+....#
+.....
+..#..
+";
+
+        var actualRound2 = Render(elves, 0, 4, 0, 5);
+        Trace.WriteLine(actualRound2);
+        Trace.WriteLine();
+        Assert.Equal(expectedRound2.ReplaceLineEndings(), actualRound2);
+
+        ExecuteRound(elves, 2);
+
+        const string expectedRound3 = @"
+..#..
+....#
+#....
+....#
+.....
+..#..
+";
+
+        var actualRound3 = Render(elves, 0, 4, 0, 5);
+        Trace.WriteLine(actualRound3);
+        Trace.WriteLine();
+        Assert.Equal(expectedRound3.ReplaceLineEndings(), actualRound3);
+
+
+        ExecuteRound(elves, 3);
+        
+        var actualRound4 = Render(elves, 0, 4, 0, 5);
+        Trace.WriteLine(actualRound4);
+        Trace.WriteLine();
+        Assert.Equal(expectedRound3.ReplaceLineEndings(), actualRound4);
     }
 
     [Fact]
