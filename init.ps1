7fdfdd351e1caab320dff1397022b0d442a115b9
James Holwell <james@abstractleap.com>
2023-11-30 21:11:17 +0000

    adjust powershell init to run from root
    and redefine commands to match preferred usage pattern

---
+++ init.ps1
@@ -0,0 +1,13 @@
+﻿function global:aoc {
+    Param([parameter(ValueFromRemainingArguments = $true)][string[]]$args)
+    dotnet run --project .\AdventOfCode @args
+}
+
+function global:buildaoc {
+    dotnet build AdventOfCode -c Release
+}
+
+function global:runaoc {
+    Param([parameter(ValueFromRemainingArguments = $true)][string[]]$args)
+    dotnet .\AdventOfCode\bin\Release\net6.0\AdventOfCode.dll @args
+}
