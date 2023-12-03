60ba6f24db6aca756c70e54e9c37e83c17c42790
James Holwell <james@abstractleap.com>
2023-12-03 06:10:12 +0000

    day 3 part 2

--- AdventOfCode.CSharp/2023/Day3.cs
+++ AdventOfCode.CSharp/2023/Day3.cs
@@ -7,7 +7,11 @@ namespace AdventOfCode.CSharp._2023;
 public class Day3 : Solver {
     public Day3(string? input = null, ITestOutputHelper? outputHelper = null) : base(input, outputHelper) { }
 
-    public override long SolvePartOne() {
+    public override long SolvePartOne() => Solve().SumPartNumbers;
+
+    public override long SolvePartTwo() => Solve().SumGearRatios;
+
+    public (int SumPartNumbers, int SumGearRatios) Solve() {
         var lines = Shared.Split(Input).Select(l => l.ToCharArray()).ToArray();
 
         var h = lines.Length + 2;
@@ -22,7 +26,8 @@ public class Day3 : Solver {
                 schematic[y, x] = lines[y - 1][x - 1];
 
         var buffer = new StringBuilder(w);
-        var accumulator = 0;
+        var partNumberAccumulator = 0;
+        var asterisks = new Dictionary<int, List<int>>();
         
         for (var y = 0; y < h; ++y) {
             buffer.Clear();
@@ -41,22 +46,56 @@ public class Day3 : Solver {
                     isSymbol |= schematic[y, x] != '.'; // right-inline
                     isSymbol |= schematic[y, x - buffer.Length - 1] != '.'; // left-inline
                     for (var ix = x - buffer.Length - 1; ix <= x; ++ix) {
-                        isSymbol |= schematic[y-1, ix] != '.'; // aboxe
+                        isSymbol |= schematic[y-1, ix] != '.'; // above
                         isSymbol |= schematic[y+1, ix] != '.'; // below
+                    }
+
+                    if (schematic[y, x] == '*') {
+                        // right-inline
+                        if (!asterisks.ContainsKey(y * w + x))
+                            asterisks[y * w + x] = new List<int>();
+                        asterisks[y * w + x].Add(value);
+                    }
+
+                    if (schematic[y, x - buffer.Length - 1] == '*') {
+                        var ay = y;
+                        var ax = x - buffer.Length - 1;
+                        // left-inline
+                        if (!asterisks.ContainsKey(ay * w + ax))
+                            asterisks[ay * w + ax] = new List<int>();
+                        asterisks[ay * w + ax].Add(value);
+                    }
+
+                    for (var ix = x - buffer.Length - 1; ix <= x; ++ix) {
+                        if (schematic[y - 1, ix] == '*') {
+                            var ay = y - 1;
+                            // above
+                            if (!asterisks.ContainsKey(ay * w + ix))
+                                asterisks[ay * w + ix] = new List<int>();
+                            asterisks[ay * w + ix].Add(value);
+                        }
+                        
+                        if (schematic[y + 1, ix] == '*') {
+                            var ay = y + 1;
+                            // above
+                            if (!asterisks.ContainsKey(ay * w + ix))
+                                asterisks[ay * w + ix] = new List<int>();
+                            asterisks[ay * w + ix].Add(value);
+                        }
                     }
                     
                     //Output.WriteLine(isSymbol);
-                    if (isSymbol) accumulator += value;
+                    if (isSymbol) partNumberAccumulator += value;
                     buffer.Clear();
                 }
             }
         }
 
-        return accumulator;
+        var sumGearRatios = asterisks.Where(a => a.Value.Count == 2)
+            .Sum(a => a.Value.First() * a.Value.Last());
+        return (partNumberAccumulator, sumGearRatios);
     }
-
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
-
+    
     private const string? ExampleInput = @"
 467..114..
 ...*......
@@ -74,5 +113,11 @@ public class Day3 : Solver {
     public void SolvesPartOneExample() {
         var actual = new Day3(ExampleInput, Output).SolvePartOne();
         Assert.Equal(4361, actual);
+    }
+    
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = new Day3(ExampleInput, Output).SolvePartTwo();
+        Assert.Equal(467835, actual);
     }
 }
