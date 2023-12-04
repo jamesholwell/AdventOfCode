c1cdad6c9c8fb497a95fb3e6e7574947fd64dfab
James Holwell <james@abstractleap.com>
2023-12-04 09:25:08 +0000

    day 4 part 2

--- AdventOfCode.CSharp/2023/Day4.cs
+++ AdventOfCode.CSharp/2023/Day4.cs
@@ -25,8 +25,29 @@ public class Day4 : Solver {
         return accumulator;
     }
 
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
+    public override long SolvePartTwo() {
+        var lines = Shared.Split(Input);
+        var accumulator = Enumerable.Repeat(1, lines.Length).ToArray();
 
+        for (var i = 0; i < lines.Length; ++ i) {
+            var line = lines[i];
+            var outerparts = line.Split(':'); 
+            var parts = outerparts[1].Split('|');
+            var winningNumbers = parts[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var myNumbers = parts[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var matches = myNumbers.Where(n => winningNumbers.Contains(n));
+            var numberOfMatches = matches.Count();
+
+            if (numberOfMatches > 0) {
+                for (var ii = i + 1; ii < i + 1 + numberOfMatches; ++ii) {
+                    accumulator[ii] += accumulator[i];
+                }
+            }
+        }
+
+        return accumulator.Sum();
+    }
     private const string? ExampleInput = @"
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -40,5 +61,11 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     public void SolvesPartOneExample() {
         var actual = new Day4(ExampleInput, Output).SolvePartOne();
         Assert.Equal(13, actual);
+    }
+
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = new Day4(ExampleInput, Output).SolvePartTwo();
+        Assert.Equal(30, actual);
     }
 }
