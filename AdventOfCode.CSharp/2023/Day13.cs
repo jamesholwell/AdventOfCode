7f956ca5ca5f9e27a63e45b1833a2ad7e9e4770f
James Holwell <james@abstractleap.com>
2023-12-13 15:01:28 +0000

    day 13 part 2

--- AdventOfCode.CSharp/2023/Day13.cs
+++ AdventOfCode.CSharp/2023/Day13.cs
@@ -38,7 +38,7 @@ public class Day13 : Solver {
         return sb.ToString();
     }
 
-    private static int FindHorizontalMirror(string pattern) {
+    private static int FindHorizontalMirror(string pattern, int excludeMirror = 0) {
         var rows = Shared.Split(pattern)
             .Select((r, i) => (r, i))
             .ToDictionary(p => p.i, p => p.r);
@@ -69,7 +69,9 @@ public class Day13 : Solver {
                             .ToArray());
 
         var mirrors = candidateMirrorsAndMatches
-            .Where(p => p.Value.All(r => identicalRows.Contains(r)))
+            .Where(p =>
+                p.Key != excludeMirror - 1
+                && p.Value.All(r => identicalRows.Contains(r)))
             .ToArray();
 
         return mirrors.Length switch {
@@ -79,7 +81,40 @@ public class Day13 : Solver {
         };
     }
 
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
+    public override long SolvePartTwo() {
+        var patterns = Input.SplitBy("\n\n");
+        var accumulator = 0;
+
+        foreach (var pattern in patterns) {
+            var existingHMirror = FindHorizontalMirror(pattern);
+            var existingVMirror = FindHorizontalMirror(Rotate(pattern));
+            var patternBuilder = new StringBuilder(pattern.Length);
+            
+            for (var i = 0; i < pattern.Length; ++i) {
+                if (pattern[i] != '.' && pattern[i] != '#') continue;
+
+                patternBuilder.Clear();
+                patternBuilder.Append(pattern);
+                patternBuilder.Remove(i, 1);
+                patternBuilder.Insert(i, pattern[i] == '.' ? '#' : '.');
+                var newPattern = patternBuilder.ToString();
+                    
+                var newHMirror = FindHorizontalMirror(newPattern, existingHMirror);
+                if (newHMirror > 0) {
+                    accumulator += 100 * newHMirror;
+                    break;
+                }
+
+                var newVMirror = FindHorizontalMirror(Rotate(newPattern), existingVMirror);
+                if (newVMirror > 0) {
+                    accumulator += newVMirror;
+                    break;
+                }
+            }
+        }
+
+        return accumulator;
+    }
 
     private const string? ExampleInput = @"
 #.##..##.
@@ -103,5 +138,11 @@ public class Day13 : Solver {
     public void SolvesPartOneExample() {
         var actual = new Day13(ExampleInput, Output).SolvePartOne();
         Assert.Equal(405, actual);
+    }
+    
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = new Day13(ExampleInput, Output).SolvePartTwo();
+        Assert.Equal(400, actual);
     }
 }
