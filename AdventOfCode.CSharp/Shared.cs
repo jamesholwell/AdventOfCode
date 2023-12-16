2043f1448286a66edbc6cff138e6d227e609112d
James Holwell <james@abstractleap.com>
2023-12-16 10:57:15 +0000

    add split to grid helper

--- AdventOfCode.CSharp/Shared.cs
+++ AdventOfCode.CSharp/Shared.cs
@@ -8,4 +8,17 @@ public static class Shared {
         s.SplitBy("\n");
 
     public static int[] SplitInt(this string s) => Shared.Split(s).Select(int.Parse).ToArray();
+
+    public static char[,] SplitGrid(this string s) {
+        var rows = Split(s);
+        var height = rows.Length;
+        var width = rows[0].Length;
+        var grid = new char[height, width];
+
+        for (var y = 0; y < height; ++y)
+        for (var x = 0; x < width; ++x)
+            grid[y, x] = rows[y][x];
+
+        return grid;
+    }
 }
