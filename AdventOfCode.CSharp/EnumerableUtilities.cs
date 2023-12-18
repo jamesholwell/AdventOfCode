959806e585493ddb84c53495394be485010bdbb0
James Holwell <james@abstractleap.com>
2023-12-18 21:45:42 +0000

    add pairwise on enumerables

---
+++ AdventOfCode.CSharp/EnumerableUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.CSharp;
+
+public static class EnumerableUtilities {
+    public static IEnumerable<Tuple<T, T>> Pairwise<T>(this IEnumerable<T> enumerable) {
+        using var e = enumerable.GetEnumerator();
+        if (!e.MoveNext()) yield break;
+
+        var previous = e.Current;
+        while (e.MoveNext())
+            yield return Tuple.Create(previous, previous = e.Current);
+    }
+}
