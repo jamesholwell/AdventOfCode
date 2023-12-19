7242c2f18ba0890df48133255c8e3ed659154572
James Holwell <james@abstractleap.com>
2023-12-19 11:08:20 +0000

    day 19 part 1

--- AdventOfCode.CSharp/2023/Day19.cs
+++ AdventOfCode.CSharp/2023/Day19.cs
@@ -12,77 +12,43 @@ public class Day19 : Solver {
 
     private enum Operator {
         None,
+
         LessThan,
+
         GreaterThan
     }
-    
+
+    private enum Variable {
+        x,
+
+        m,
+
+        a,
+
+        s
+    }
+
+    private record Rule {
+        public Variable v;
+
+        public Operator o;
+
+        public int t;
+
+        public string d = string.Empty;
+    }
+
     private record Workflow {
-        // precedences
-        public int xp;
-        public int mp;
-        public int ap;
-        public int sp;
-        
-        // operators
-        public Operator xo;
-        public Operator mo;
-        public Operator ao;
-        public Operator so;
-        
-        // thresholds
-        public int xt;
-        public int mt;
-        public int at;
-        public int st;
-        
-        // destinations
-        public string xd = string.Empty;
-        public string md = string.Empty;
-        public string ad = string.Empty;
-        public string sd = string.Empty;
+        public List<Rule> Rules = new List<Rule>();
+
         public string dd = string.Empty;
 
         public override string ToString() {
             var sb = new StringBuilder();
             sb.Append("{");
 
-            for (var i = 1; i < 5; ++i) {
-                // get operator (if exists)
-                var o =
-                      (xp == i) ? xo
-                    : (mp == i) ? mo
-                    : (ap == i) ? ao
-                    : (sp == i) ? so
-                    : Operator.None;
-
-                if (o == Operator.None)
-                    break;
-                
-                // get variable
-                var v =
-                      (xp == i) ? "x"
-                    : (mp == i) ? "m"
-                    : (ap == i) ? "a"
-                    : (sp == i) ? "s"
-                    : throw new InvalidOperationException();
-                
-                // get threshold 
-                var t =
-                      (xp == i) ? xt
-                    : (mp == i) ? mt
-                    : (ap == i) ? at
-                    : (sp == i) ? st
-                    : throw new InvalidOperationException();
-
-                // rule matches, move
-                var d =
-                      (xp == i) ? xd
-                    : (mp == i) ? md
-                    : (ap == i) ? ad
-                    : (sp == i) ? sd
-                    : throw new InvalidOperationException();
-
-                sb.Append($"{v}{(o == Operator.GreaterThan ? ">" : "<")}{t}:{d},");
+            foreach (var rule in Rules) {
+                sb.Append($"{rule.v}{(rule.o == Operator.GreaterThan ? ">" : "<")}{rule.t}:{rule.d},");
             }
 
             sb.Append(dd);
@@ -90,18 +56,21 @@ public class Day19 : Solver {
             return sb.ToString();
         }
     }
-    
+
     private record Part {
         public int x;
+
         public int m;
+
         public int a;
+
         public int s;
 
         public override string ToString() {
             return $"x={x},m={m},a={a},s={s}";
         }
     }
-    
+
     public override long SolvePartOne() {
         var inputSections = Input.SplitBy("\n\n");
         var workflowSection = inputSections[0].Split('\n');
@@ -111,64 +80,43 @@ public class Day19 : Solver {
         foreach (var workflowInput in workflowSection) {
             var bracePosition = workflowInput.IndexOf('{');
             var k = workflowInput[0..bracePosition];
-            
-            var workflowParts = workflowInput[(bracePosition+1)..^1].Split(',');
-            
+
+            var workflowParts = workflowInput[(bracePosition + 1)..^1].Split(',');
+
             var w = new Workflow {
                 dd = workflowParts[^1]
             };
 
-            var i = 0;
             foreach (var ruleInput in workflowParts[0..^1]) {
                 var ruleParts = ruleInput.Split(':');
-                var v = ruleParts[0][0];
+                var v = ruleParts[0][0] switch {
+                    'x' => Variable.x,
+                    'm' => Variable.m,
+                    'a' => Variable.a,
+                    's' => Variable.s,
+                    _ => throw new InvalidOperationException()
+                };
                 var o = ruleParts[0][1] == '<' ? Operator.LessThan : Operator.GreaterThan;
                 var t = int.Parse(ruleParts[0][2..]);
                 var d = ruleParts[1];
 
-                switch (v) {
-                    case 'x':
-                        w.xp = ++i;
-                        w.xo = o;
-                        w.xt = t;
-                        w.xd = d;
-                        break;
-                    case 'm':
-                        w.mp = ++i;
-                        w.mo = o;
-                        w.mt = t;
-                        w.md = d;
-                        break;
-                    case 'a':
-                        w.ap = ++i;
-                        w.ao = o;
-                        w.at = t;
-                        w.ad = d;
-                        break;
-                    case 's':
-                        w.sp = ++i;
-                        w.so = o;
-                        w.st = t;
-                        w.sd = d;
-                        break;
-                }
+                w.Rules.Add(new Rule { v = v, o = o, t = t, d = d });
             }
             
-            workflows.Add(k, w);
-            Trace.WriteLine(workflowInput);
-            Trace.WriteLine($"-> {k}{w}");
             Debug.Assert(workflowInput == k + w);
+            workflows.Add(k, w);
         }
-        
+
         var parts = new List<Part>(partsSection.Length);
         foreach (var partInput in partsSection) {
             var partParts = partInput[1..^1].Split(',');
-            parts.Add(new Part {
-                x = int.Parse(partParts[0][2..]),
-                m = int.Parse(partParts[1][2..]),
-                a = int.Parse(partParts[2][2..]),
-                s = int.Parse(partParts[3][2..]),
-            });
+            parts.Add(
+                new Part {
+                    x = int.Parse(partParts[0][2..]),
+                    m = int.Parse(partParts[1][2..]),
+                    a = int.Parse(partParts[2][2..]),
+                    s = int.Parse(partParts[3][2..]),
+                });
         }
 
         var accept = new List<Part>();
@@ -179,56 +127,30 @@ public class Day19 : Solver {
 
             while (currentLocation != "A" && currentLocation != "R") {
                 queue.Enqueue(currentLocation);
-                
+
                 var workflow = workflows[currentLocation];
                 currentLocation = workflow.dd;
 
-                for (var i = 1; i < 5; ++i) {
-                    // get operator (if exists)
-                    var o = 
-                          (workflow.xp == i) ? workflow.xo
-                        : (workflow.mp == i) ? workflow.mo
-                        : (workflow.ap == i) ? workflow.ao
-                        : (workflow.sp == i) ? workflow.so
-                        : Operator.None;
+                foreach (var rule in workflow.Rules) {
+                    var v =
+                        (rule.v == Variable.x) ? part.x
+                        : (rule.v == Variable.m) ? part.m
+                        : (rule.v == Variable.a) ? part.a
+                        : (rule.v == Variable.s) ? part.s
+                        : throw new InvalidOperationException();
 
-                    if (o == Operator.None) 
+                    if (rule.o == Operator.LessThan && v < rule.t || rule.o == Operator.GreaterThan & v > rule.t) {
+                        currentLocation = rule.d;
                         break;
-                    
-                    // get threshold and value
-                    var t = 
-                          (workflow.xp == i) ? workflow.xt
-                        : (workflow.mp == i) ? workflow.mt
-                        : (workflow.ap == i) ? workflow.at
-                        : (workflow.sp == i) ? workflow.st
-                        : throw new InvalidOperationException();
-                    
-                    var v = 
-                          (workflow.xp == i) ? part.x
-                        : (workflow.mp == i) ? part.m
-                        : (workflow.ap == i) ? part.a
-                        : (workflow.sp == i) ? part.s
-                        : throw new InvalidOperationException();
-
-                    // test rule
-                    if (o == Operator.GreaterThan && v <= t || o == Operator.LessThan & v >= t)
-                        continue;
-                    
-                    // rule matches, move
-                    currentLocation = 
-                          (workflow.xp == i) ? workflow.xd
-                        : (workflow.mp == i) ? workflow.md
-                        : (workflow.ap == i) ? workflow.ad
-                        : (workflow.sp == i) ? workflow.sd
-                        : throw new InvalidOperationException();
+                    }
                 }
             }
-            
+
             queue.Enqueue(currentLocation);
 
             if (currentLocation == "A")
                 accept.Add(part);
-            
+
             Trace.WriteLine($"{part}: {string.Join(" -> ", queue)}");
         }
 
