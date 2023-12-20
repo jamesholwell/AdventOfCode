1340bbd9e1703eb24c8c3bae5cf74336a6a2449f
James Holwell <james@abstractleap.com>
2023-12-20 23:03:45 +0000

    add maths utility to hold lcm

---
+++ AdventOfCode.CSharp/Maths.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.CSharp;
+
+public static class Maths {
+    // ReSharper disable once InconsistentNaming - this is the mathematical name
+    public static long lcm(long a, long b) {
+        return (a / gcf(a, b)) * b;
+    }
+
+    // ReSharper disable once InconsistentNaming - this is the mathematical name
+    private static long gcf(long a, long b) {
+        while (b != 0) {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
