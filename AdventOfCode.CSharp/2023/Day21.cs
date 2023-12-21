3b94742ce0ad1f1d2129c303f43d19de4415532b
James Holwell <james@abstractleap.com>
2023-12-21 20:52:40 +0000

    day 21 part 2 (naive)

--- AdventOfCode.CSharp/2023/Day21.cs
+++ AdventOfCode.CSharp/2023/Day21.cs
@@ -6,11 +6,16 @@ namespace AdventOfCode.CSharp._2023;
 public class Day21 : Solver {
     public Day21(string? input = null, ITestOutputHelper? outputHelper = null) : base(input, outputHelper) { }
 
-    public override long SolvePartOne() => SolvePartOne(64);
+    public override long SolvePartOne() => Solve(64);
 
-    private long SolvePartOne(int steps) {
+    public override long SolvePartTwo() => Solve(26501365);
+
+    private long Solve(int steps) {
         var points = Input.SplitPoints();
-        var plots = new HashSet<(int x, int y)>(points.CoordinatesWhere(c => c == '.'));
+        var w = points.Max(p => p.x) + 1;
+        var h = points.Max(p => p.y) + 1;
+        
+        var plots = new HashSet<(int x, int y)>(points.CoordinatesWhere(c => c == '.' || c == 'S'));
         var reachableEvens = new HashSet<(int x, int y)>();
         var reachableOdds = new HashSet<(int x, int y)>();
 
@@ -32,42 +37,46 @@ public class Day21 : Solver {
                 reachableOdds.Add(position);
             
             var up = (position.x, position.y - 1);
-            if (!seenPositions.Contains(up) && plots.Contains(up)) {
+            if (!seenPositions.Contains(up) && plots.Contains((Maths.wrap(position.x, w), Maths.wrap(position.y - 1, h)))) {
                 seenPositions.Add(up);
                 queue.Enqueue(up, t + 1);
             }
 
             var right = (position.x + 1, position.y);
-            if (!seenPositions.Contains(right) && plots.Contains(right)) {
+            if (!seenPositions.Contains(right) && plots.Contains((Maths.wrap(position.x + 1, w), Maths.wrap(position.y, h)))) {
                 seenPositions.Add(right);
                 queue.Enqueue(right, t + 1 );
             }
             
             var down = (position.x, position.y + 1);
-            if (!seenPositions.Contains(down) && plots.Contains(down)) {
+            if (!seenPositions.Contains(down) && plots.Contains((Maths.wrap(position.x, w), Maths.wrap(position.y + 1, h)))) {
                 seenPositions.Add(down);
                 queue.Enqueue(down, t + 1 );
             }
             
             var left = (position.x - 1, position.y);
-            if (!seenPositions.Contains(left) && plots.Contains(left)) {
+            if (!seenPositions.Contains(left) && plots.Contains((Maths.wrap(position.x - 1, w), Maths.wrap(position.y, h)))) {
                 seenPositions.Add(left);
                 queue.Enqueue(left, t + 1);
             }
         }
 
         var reachable = steps % 2 == 0 ? reachableEvens : reachableOdds;
-        
+
         // render grid
-        var grid = new PointGrid<(int x, int y, char c)>(points, p => p.x, p => p.y, p => p.c);
-        foreach (var point in reachable)
-            grid.Set(point.x, point.y, 'O');
-        Trace.WriteLine(grid.Render());
-        
+        if (steps < 51) {
+            var grid = new PointGrid<(int x, int y)>(reachable, p => p.x, p => p.y);
+
+            grid.Fill((x, y) =>
+                start.x == x && start.y == y ? 'S' :
+                reachable.Contains((x, y)) ? 'O' :
+                plots.Contains((Maths.wrap(x, w), Maths.wrap(y, h))) ? '.' : '#');
+
+            Trace.WriteLine(grid.Render());
+        }
+
         return reachable.Count;
     }
-    
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
 
     private const string? ExampleInput = @"
 ...........
@@ -90,7 +99,20 @@ public class Day21 : Solver {
     [InlineData(3, 6)]
     [InlineData(6, 16)]
     public void SolvesPartOneExamples(int steps, int expected) {
-        var actual = new Day21(ExampleInput, Output).SolvePartOne(steps);
+        var actual = new Day21(ExampleInput, Output).Solve(steps);
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData(6, 16)]
+    [InlineData(10, 50)]
+    [InlineData(50, 1594)]
+    [InlineData(100, 6536)]
+    [InlineData(500, 167004)]
+    //[InlineData(1000, 668697)] - works but is slow
+    //[InlineData(5000, 16733044)] - works but is slow
+    public void SolvesPartTwoExamples(int steps, int expected) {
+        var actual = new Day21(ExampleInput, Output).Solve(steps);
         Assert.Equal(expected, actual);
     }
 }
