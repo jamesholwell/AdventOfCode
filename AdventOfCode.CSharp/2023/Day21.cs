4f458c7d16f9d4e0821abdb49eec6abfd20b9e63
James Holwell <james@abstractleap.com>
2023-12-21 20:01:05 +0000

    day 21 part 1 (handle more cases)

--- AdventOfCode.CSharp/2023/Day21.cs
+++ AdventOfCode.CSharp/2023/Day21.cs
@@ -8,13 +8,11 @@ public class Day21 : Solver {
 
     public override long SolvePartOne() => SolvePartOne(64);
 
-    private long SolvePartOne(int maximumSteps) {
-        if (maximumSteps % 2 != 0)
-            throw new NotImplementedException("Algorithm only considers evenly reachable spaces");
-        
+    private long SolvePartOne(int steps) {
         var points = Input.SplitPoints();
         var plots = new HashSet<(int x, int y)>(points.CoordinatesWhere(c => c == '.'));
-        var reachablePositions = new HashSet<(int x, int y)>();
+        var reachableEvens = new HashSet<(int x, int y)>();
+        var reachableOdds = new HashSet<(int x, int y)>();
 
         var queue = new PriorityQueue<(int x, int y), int>();
         var seenPositions = new HashSet<(int x, int y)>();
@@ -25,47 +23,50 @@ public class Day21 : Solver {
         seenPositions.Add(start);
         
         while (queue.TryDequeue(out var position, out var t)) {
-            if (t > maximumSteps)
+            if (t > steps)
                 continue;
-
+            
             if (t % 2 == 0)
-                reachablePositions.Add(position);
+                reachableEvens.Add(position);
+            else
+                reachableOdds.Add(position);
             
             var up = (position.x, position.y - 1);
             if (!seenPositions.Contains(up) && plots.Contains(up)) {
                 seenPositions.Add(up);
-                queue.Enqueue(position with { y = position.y - 1 }, t + 1);
+                queue.Enqueue(up, t + 1);
             }
 
             var right = (position.x + 1, position.y);
             if (!seenPositions.Contains(right) && plots.Contains(right)) {
                 seenPositions.Add(right);
-                queue.Enqueue(position with { x = position.x + 1}, t + 1 );
+                queue.Enqueue(right, t + 1 );
             }
             
             var down = (position.x, position.y + 1);
             if (!seenPositions.Contains(down) && plots.Contains(down)) {
                 seenPositions.Add(down);
-                queue.Enqueue(position with { y = position.y + 1}, t + 1 );
+                queue.Enqueue(down, t + 1 );
             }
             
             var left = (position.x - 1, position.y);
             if (!seenPositions.Contains(left) && plots.Contains(left)) {
                 seenPositions.Add(left);
-                queue.Enqueue(position with { x = position.x - 1}, t + 1);
+                queue.Enqueue(left, t + 1);
             }
         }
+
+        var reachable = steps % 2 == 0 ? reachableEvens : reachableOdds;
         
+        // render grid
         var grid = new PointGrid<(int x, int y, char c)>(points, p => p.x, p => p.y, p => p.c);
-        
-        foreach (var point in reachablePositions)
+        foreach (var point in reachable)
             grid.Set(point.x, point.y, 'O');
-        
         Trace.WriteLine(grid.Render());
         
-        return reachablePositions.Count;
+        return reachable.Count;
     }
-
+    
     public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
 
     private const string? ExampleInput = @"
@@ -81,10 +82,15 @@ public class Day21 : Solver {
 .##..##.##.
 ...........
 ";
-
-    [Fact]
-    public void SolvesPartOneExample() {
-        var actual = new Day21(ExampleInput, Output).SolvePartOne(6);
-        Assert.Equal(16, actual);
+    
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    [InlineData(2, 4)]
+    [InlineData(3, 6)]
+    [InlineData(6, 16)]
+    public void SolvesPartOneExamples(int steps, int expected) {
+        var actual = new Day21(ExampleInput, Output).SolvePartOne(steps);
+        Assert.Equal(expected, actual);
     }
 }
