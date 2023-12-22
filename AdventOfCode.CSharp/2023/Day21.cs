1d117e9ff75fb013d83c830098c3b056d5b3fef8
James Holwell <james@abstractleap.com>
2023-12-22 17:59:17 +0000

    day 21 part 2 (algorithm starter)

--- AdventOfCode.CSharp/2023/Day21.cs
+++ AdventOfCode.CSharp/2023/Day21.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Diagnostics;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace AdventOfCode.CSharp._2023;
@@ -77,6 +78,80 @@ public class Day21 : Solver {
 
         return reachable.Count;
     }
+    
+    private long SolveDiagonal(int steps) {
+        var points = Input.SplitPoints();
+        var sz = points.Max(p => p.x) + 1;
+        Debug.Assert(sz == points.Max(p => p.y) + 1);
+
+        // check that it has the magical properties required
+        if ((steps % sz) != (sz - 1) / 2)
+            return -1;
+        
+        var plots = new HashSet<(int x, int y)>(points.CoordinatesWhere(c => c == '.' || c == 'S'));
+        var reachablePositions = new Dictionary<(int x, int y), int>();
+
+        var queue = new PriorityQueue<(int x, int y), int>();
+
+        var startingPosition = points.Single(p => p.value == 'S');
+        var start = (startingPosition.x, startingPosition.y);
+        queue.Enqueue(start, 0);
+        reachablePositions.Add(start, 0);
+        
+        while (queue.TryDequeue(out var position, out var t)) {
+            if (t > steps)
+                continue;
+            
+            var up = (position.x, position.y - 1);
+            if (!reachablePositions.ContainsKey(up) && plots.Contains(up)) {
+                reachablePositions.Add(up, t + 1);
+                queue.Enqueue(up, t + 1);
+            }
+
+            var right = (position.x + 1, position.y);
+            if (!reachablePositions.ContainsKey(right) && plots.Contains(right)) {
+                reachablePositions.Add(right, t + 1);
+                queue.Enqueue(right, t + 1);
+            }
+            
+            var down = (position.x, position.y + 1);
+            if (!reachablePositions.ContainsKey(down) && plots.Contains(down)) {
+                reachablePositions.Add(down, t + 1);
+                queue.Enqueue(down, t + 1);
+            }
+            
+            var left = (position.x - 1, position.y);
+            if (!reachablePositions.ContainsKey(left) && plots.Contains(left)) {
+                reachablePositions.Add(left, t + 1);
+                queue.Enqueue(left, t + 1);
+            }
+        }
+
+        // model the expanding diamond!
+        
+        // this is the 'number of rings' around the central piece
+        var div = (long)steps / sz;
+        
+        // this is the 'number of steps remaining' after moving to the centre of the edges
+        var rem = (long)steps % sz;
+
+        var evenTiles = (1 + div) * (1 + div);
+        var evenTileSpaces = reachablePositions.Count(p => p.Value % 2 == 0);
+        
+        var oddTiles = div * div;
+        var oddTileSpaces = reachablePositions.Count(p => p.Value % 2 == 1);
+
+        var edgeTiles = div;
+        var edgeAdditionalSpaces =reachablePositions.Count(p => p.Value % 2 == 1 && p.Value > rem);
+        
+        var pointyTiles = (div + 1);
+        var pointyOvercountedSpaces = reachablePositions.Count(p => p.Value % 2 == 0 && p.Value > rem);
+
+        return evenTiles * evenTileSpaces 
+               + oddTiles * oddTileSpaces 
+               + edgeTiles * edgeAdditionalSpaces
+               - pointyTiles * pointyOvercountedSpaces;
+    }
 
     private const string? ExampleInput = @"
 ...........
@@ -90,6 +165,30 @@ public class Day21 : Solver {
 .##.#.####.
 .##..##.##.
 ...........
+";
+    
+    private const string? TrivialInput = @"
+.....
+.....
+..S..
+.....
+.....
+";
+    
+    private const string? DiagonalInput = @"
+.............
+.........#.#.
+.##........#.
+....#........
+....#...#....
+.............
+......S......
+.............
+..#.....#....
+.......#.#...
+.##.....#..#.
+.##.......##.
+.............
 ";
     
     [Theory]
@@ -113,6 +212,40 @@ public class Day21 : Solver {
     //[InlineData(5000, 16733044)] - works but is slow
     public void SolvesPartTwoExamples(int steps, int expected) {
         var actual = new Day21(ExampleInput, Output).Solve(steps);
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData(2)]
+    [InlineData(7)]
+    [InlineData(12)]
+    [InlineData(23 * 5 + 2)]
+    public void SolvesTrivialExampleDiagonally(int steps) {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var expected = new Day21(TrivialInput, Output).Solve(steps);
+        stopwatch.Stop();
+        var bfTime = stopwatch.ElapsedTicks;
+        
+        stopwatch.Reset();
+        stopwatch.Start();
+        var actual = new Day21(TrivialInput, Output).SolveDiagonal(steps);
+        stopwatch.Stop();
+        var algoTime = stopwatch.ElapsedTicks;
+        
+        Assert.Equal(expected, actual);
+        Output.WriteLine(
+            $"Brute force took {bfTime:N0} ticks, algorithm took {algoTime:N0} ticks ({((float)bfTime / algoTime):N1}x)");
+    }
+    
+    [Theory]
+    [InlineData(6)]
+    [InlineData(19)]
+    [InlineData(32)]
+    [InlineData(23 * 13 + 6)]
+    public void SolvesDiagonalExample(int steps) {
+        var expected = new Day21(DiagonalInput, Output).Solve(steps);
+        var actual = new Day21(DiagonalInput, Output).SolveDiagonal(steps);
         Assert.Equal(expected, actual);
     }
 }
