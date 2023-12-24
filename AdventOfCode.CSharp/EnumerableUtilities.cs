8da37808c47859df73b83255725fc4d0c8b14348
James Holwell <james@abstractleap.com>
2023-12-24 11:34:48 +0000

    add combinations enumerable helper
    [1; 2; 3] -> [(1, 2); (1, 3); (2, 3)]

--- AdventOfCode.CSharp/EnumerableUtilities.cs
+++ AdventOfCode.CSharp/EnumerableUtilities.cs
@@ -9,4 +9,20 @@ public static class EnumerableUtilities {
         while (e.MoveNext())
             yield return Tuple.Create(previous, previous = e.Current);
     }
+
+    public static IEnumerable<(T left, T right)> Combinations<T>(this IEnumerable<T> enumerable) {
+        var array = enumerable as T[] ?? enumerable.ToArray();
+
+        if (array.Length < 2) 
+            return Array.Empty<(T, T)>();
+
+        var ret = new (T, T)[(array.Length * (array.Length - 1)) / 2];
+
+        var o = 0;
+        for (var i = 0; i < array.Length - 1; ++i)
+        for (var j = i + 1; j < array.Length; ++j)
+            ret[o++] = (array[i], array[j]);
+
+        return ret;
+    }
 }
