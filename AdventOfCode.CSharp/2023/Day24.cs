6951c8b577d9b5ce2c80a6cc03661e630587bb24
James Holwell <james@abstractleap.com>
2023-12-25 23:21:07 +0000

    day 24 part 2 (velocity)

--- AdventOfCode.CSharp/2023/Day24.cs
+++ AdventOfCode.CSharp/2023/Day24.cs
@@ -66,7 +66,87 @@ public class Day24 : Solver {
         return accumulator;
     }
 
-    public override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
+    public override long SolvePartTwo() {
+        var stones = Parse(Input);
+
+        /*
+
+         Position of rock and left/right particle are the same at t1/(t1+n) for n in Z
+         (1) left.px + left.vx*t1       = rock.px + rock.vx*t1
+         (2) right.px + right.vx*(t1+n) = rock.px + rock.vx*(t1+n)
+
+         Subtract (1) from (2):
+         (right.px - left.px) + (right.vx - left.vx)*t1 + right.vx*n = rock.vx*n
+
+         Since right.vx == left.vx:
+         (right.px - left.px) = (rock.vx - left.vx)*n
+
+         Then:
+         (right.px - left.px) - (rock.vx - left.vx)*n = 0
+
+         So:
+         (right.px - left.px) = 0 [mod (rock.vx - left.vx)]
+
+         */
+        
+        // upon inspection, this is a "good enough" possibility set
+        var possibleVx = Enumerable.Range(-1000, 2000).ToHashSet();
+        var possibleVy = Enumerable.Range(-1000, 2000).ToHashSet();
+        var possibleVz = Enumerable.Range(-1000, 2000).ToHashSet();
+        
+        // search x velocity constraints
+        //Trace.WriteLine("Searching for stones parallel in x");
+        foreach (var pair in stones.GroupBy(s => s.vx).Where(g => g.Count() > 1).SelectMany(g => g.Combinations())) {
+            //Trace.WriteLine($"Stones {pair.left.l}, {pair.right.l} are parallel with vx = {pair.left.vx} and d = {pair.right.px - pair.left.px}");
+            
+            var vx = pair.left.vx;
+            var dx = pair.right.px - pair.left.px;
+            
+            for (var i = -1000; i <= 2000; ++i)
+                if (i == vx || dx % (i - vx) != 0)
+                    possibleVx.Remove(i);
+        }
+
+        Output.WriteLine(possibleVx.Count == 1
+            ? $"Found vx = {possibleVx.Single()}"
+            : $"Found {possibleVx.Count} possible vxs {string.Join(", ", possibleVx)}");
+
+        // search y velocity constraints
+        //Trace.WriteLine("Searching for stones parallel in y");
+        foreach (var pair in stones.GroupBy(s => s.vy).Where(g => g.Count() > 1).SelectMany(g => g.Combinations())) {
+            //Trace.WriteLine($"Stones {pair.left.l}, {pair.right.l} are parallel with vy = {pair.left.vy} and d = {pair.right.py - pair.left.py}");
+            
+            var vy = pair.left.vy;
+            var dy = pair.right.py - pair.left.py;
+            
+            for (var i = -1000; i <= 2000; ++i)
+                if (i == vy || dy % (i - vy) != 0)
+                    possibleVy.Remove(i);
+        }
+
+        Output.WriteLine(possibleVy.Count == 1
+            ? $"Found vy = {possibleVy.Single()}"
+            : $"Found {possibleVy.Count} possible vys {string.Join(", ", possibleVy)}");
+
+        // search z velocity constraints
+        //Trace.WriteLine("Searching for stones parallel in z");
+        foreach (var pair in stones.GroupBy(s => s.vz).Where(g => g.Count() > 1).SelectMany(g => g.Combinations())) {
+            //Trace.WriteLine($"Stones {pair.left.l}, {pair.right.l} are parallel with vz = {pair.left.vz} and d = {pair.right.pz - pair.left.pz}");
+            
+            var vz = pair.left.vz;
+            var dz = pair.right.pz - pair.left.pz;
+            
+            for (var i = -1000; i <= 2000; ++i)
+                if (i == vz || dz % (i - vz) != 0)
+                    possibleVz.Remove(i);
+        }
+
+        Output.WriteLine(possibleVz.Count == 1
+            ? $"Found vz = {possibleVz.Single()}"
+            : $"Found {possibleVz.Count} possible vzs {string.Join(", ", possibleVz)}");
+
+        return possibleVx.Count;
+    }
 
     private Hailstone[] Parse(string input) {
         var lines = Shared.Split(input);
@@ -74,8 +154,8 @@ public class Day24 : Solver {
 
         for (var i = 0; i < lines.Length; ++i) {
             var parts = lines[i].Replace('@', ',').Split(',', StringSplitOptions.TrimEntries);
-            stones[i] = new Hailstone
-            {
+            stones[i] = new Hailstone {
+                l = Label(i),
                 px = long.Parse(parts[0]),
                 py = long.Parse(parts[1]),
                 pz = long.Parse(parts[2]),
@@ -88,8 +168,13 @@ public class Day24 : Solver {
         return stones;
     }
 
+    private static string Label(int i) => i > 675
+        ? new string(new[] { (char)((i - 676) / 26 + 'a'), (char)(i % 26 + 'A') })
+        : new string(new[] { (char)(i / 26 + 'A'), (char)(i % 26 + 'A') });
+    
     [SuppressMessage("ReSharper", "InconsistentNaming")] // these are their puzzle names
     private record Hailstone {
+        public string l;
         public long px;
         public long py;
         public long pz;
@@ -100,6 +185,8 @@ public class Day24 : Solver {
         public override string ToString() {
             return $"{px}, {py}, {pz} @ {vx}, {vy}, {vz}";
         }
+        
+        public static string Label(Hailstone h) => h.l;
     }
 
     private const string? ExampleInput = @"
@@ -176,5 +263,11 @@ Hailstones' paths crossed in the past for both hailstones.
     public void SolvesPartOneExample() {
         var actual = SolvePartOne(Parse(ExampleInput!), 7, 27, Trace);
         Assert.Equal(2, actual);
+    }
+    
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = new Day24(ExampleInput, Output).SolvePartTwo();
+        Assert.Equal(47, actual);
     }
 }
