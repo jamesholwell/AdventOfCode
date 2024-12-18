305af3c988f6e3402e823368b89c131545e84f0a
James Holwell <james@abstractleap.com>
2024-12-18 10:13:44 +0000

    2024 day 18 part 2

--- AdventOfCode.CSharp/2024/Day18.cs
+++ AdventOfCode.CSharp/2024/Day18.cs
@@ -8,21 +8,19 @@ using Xunit.Abstractions;
 namespace AdventOfCode.CSharp._2024;
 
 public class Day18(string? input = null, ITestOutputHelper? outputHelper = null)
-    : Solver(input, outputHelper) {
+    : Solver<string>(input, outputHelper) {
     private static (int x, int y)[] Parse(string input) =>
         Shared.Split(input)
             .Select(s => s.Split(','))
             .Select(s => (int.Parse(s[0]), int.Parse(s[1])))
             .ToArray();
 
-    private long SolvePartOne(string input, int numberOfBytes, int width, int height) {
-        var coords = Parse(input);
-        
+    private long SolvePartOne((int x, int y)[] bytes, int numberOfBytes, int width, int height) {
         var grid = new char[height, width];
         grid.Initialize('.');
 
         for (var i = 0; i < numberOfBytes; i++) 
-            grid[coords[i].y, coords[i].x] = '#';
+            grid[bytes[i].y, bytes[i].x] = '#';
         
         Trace.WriteLine(grid.Render());
         
@@ -44,9 +42,43 @@ public class Day18(string? input = null, ITestOutputHelper? outputHelper = null)
         }
     }
 
-    protected override long SolvePartOne() => SolvePartOne(Input, 1024, 71, 71);
+    private string SolvePartTwo((int x, int y)[] bytes, int width, int height) {
+        var traversableAddresses = new HashSet<(int, int)>(
+            Enumerable.Range(0, width).SelectMany(x => Enumerable.Range(0, height).Select(y => (x, y))));
 
-    protected override long SolvePartTwo() => throw new NotImplementedException("Solve part 1 first");
+        for (var i = 0; i < bytes.Length; ++i) {
+            traversableAddresses.Remove((bytes[i].x, bytes[i].y));
+
+            var d = Algorithm.Dijkstra(
+                startNode: (0, 0),
+                nodes: traversableAddresses,
+                labelFunc: p => p,
+                connectionFunc: TraversableNeighbors);
+
+            if (d[(width - 1, height - 1)] == int.MaxValue)
+                return $"{bytes[i].x},{bytes[i].y}";
+        }
+
+        return "oof.";
+
+        IEnumerable<((int x, int y), int d)> TraversableNeighbors((int x, int y) p) {
+            var up = p.Up();
+            if (traversableAddresses.Contains(up)) yield return (up, 1);
+            
+            var right = p.Right();
+            if (traversableAddresses.Contains(right)) yield return (right, 1);
+            
+            var down = p.Down();
+            if (traversableAddresses.Contains(down)) yield return (down, 1);
+            
+            var left = p.Left();
+            if (traversableAddresses.Contains(left)) yield return (left, 1);
+        }
+    }
+
+    protected override string SolvePartOne() => SolvePartOne(Parse(Input), 1024, 71, 71).ToString();
+
+    protected override string SolvePartTwo() => SolvePartTwo(Parse(Input), 71, 71);
 
     private const string ExampleInput = 
         """
@@ -79,7 +111,13 @@ public class Day18(string? input = null, ITestOutputHelper? outputHelper = null)
 
     [Fact]
     public void SolvesPartOneExample() {
-        var actual = SolvePartOne(ExampleInput, 12, 7, 7);
+        var actual = SolvePartOne(Parse(ExampleInput), 12, 7, 7);
         Assert.Equal(22, actual);
+    }
+    
+    [Fact]
+    public void SolvesPartTwoExample() {
+        var actual = SolvePartTwo(Parse(ExampleInput), 7, 7);
+        Assert.Equal("6,1", actual);
     }
 }
