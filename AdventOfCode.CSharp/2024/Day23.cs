ae451df2f4196e997e2686c2bf8aa19740097667
James Holwell <james@abstractleap.com>
2024-12-23 09:56:25 +0000

    2024 day 23 part 2 (faster)

--- AdventOfCode.CSharp/2024/Day23.cs
+++ AdventOfCode.CSharp/2024/Day23.cs
@@ -35,11 +35,16 @@ public class Day23(string? input = null, ITestOutputHelper? outputHelper = null)
 
     protected override string SolvePartOne() {
         var connections = GetConnections();
+
+        return GetTriplets(connections).Count.ToString();
+    }
+
+    private static HashSet<string> GetTriplets(FrozenDictionary<string, FrozenSet<string>> connections) {
         var triplets = new HashSet<string>();
 
         foreach (var connection in connections.Where(p => p.Key.StartsWith('t'))) {
             var node = connection.Value.ToArray();
-            
+
             for (var i = 0; i < node.Length; ++i) {
                 for (var j = i + 1; j < node.Length; ++j) {
                     if (connections[node[i]].Contains(node[j]))
@@ -48,50 +53,76 @@ public class Day23(string? input = null, ITestOutputHelper? outputHelper = null)
             }
         }
 
-        return triplets.Count.ToString();
+        return triplets;
     }
 
     protected override string SolvePartTwo() {
         var connections = GetConnections();
-        var i = 0;
-        var longestSet = string.Empty;
+        
+        // hey, we already did some of the work in part 1!
+        var seeds = GetTriplets(connections)
+            .Select(t => (key: t, working: t.Split(',')))
+            .Select(i => (
+                i.key,
+                i.working, 
+                remaining: connections[i.working[0]].Except(i.working).ToArray()));
 
         var seen = new HashSet<string>();
-        var queue = new Queue<string[]>(Shared.Split(Input).Select(s => s.Split('-')));
+        var queue = new Queue<(string key, string[] working, string[] remaining)>(seeds);
+        var longestSet = string.Empty;
 
-        while (queue.TryDequeue(out var workingSet)) {
-            // calculate the set key
-            Array.Sort(workingSet);
-            var key = string.Join(',', workingSet);
-            
-            // ensure we are novel
-            if (!seen.Add(key))
-                continue;
-            
-            // little progress report
-            if (++i % 10000 == 0)
-                Output.WriteLine($"i = {i}, queue = {queue.Count}, current = {key}");
-
+        while (queue.TryDequeue(out var item)) {
             // calculate the options for the next step
-            var remainingConnections = connections[workingSet[0]].Except(workingSet);
             var didQueue = false;
 
-            foreach (var nextNode in remainingConnections) {
-                if (connections[nextNode].IsSupersetOf(workingSet)) {
-                    queue.Enqueue(workingSet.Concat([nextNode]).ToArray());
-                    didQueue = true;
-                }
+            foreach (var nextNode in item.remaining) {
+                if (!connections[nextNode].IsSupersetOf(item.working))
+                    continue;
+
+                /*
+                 * The following is equivalent to
+                 *  
+                 *      var nextWorking = item.working.Concat([nextNode]).ToArray();
+                 *      Array.Sort(nextWorking);
+                 *  
+                 *      var nextKey = string.Join(',', nextWorking);
+                 *
+                 *  But leverages the already-ordered nature of the working set
+                 * 
+                 */
+                
+                // fast initialise next working set
+                var nextWorking = new string[item.working.Length + 1];
+                var place = 0;
+                while (place < item.working.Length && string.Compare(item.working[place], nextNode, StringComparison.Ordinal) < 0)
+                    ++place;
+                Array.Copy(item.working, nextWorking, place);
+                nextWorking[place] = nextNode;
+                if (place <= item.working.Length)
+                    Array.Copy(item.working, place, nextWorking, place + 1, item.working.Length - place);
+                    
+                // fast initialise next key
+                var nextKey =
+                    place == item.working.Length
+                        ? $"{item.key},{nextNode}"
+                        : $"{item.key[..(place * 3)]}{nextNode},{item.key[(place * 3)..]}";
+                
+                // test if we've already seen this set
+                if (seen.Add(nextKey))
+                    queue.Enqueue((nextKey, nextWorking, item.remaining[1..]));
+                    
+                didQueue = true;
             }
 
             // if we couldn't get any bigger, see if we were the biggest so far
-            if (!didQueue && key.Length > longestSet.Length) 
-                longestSet = key;
+            if (!didQueue && item.key.Length > longestSet.Length)
+                longestSet = item.key;
         }
 
         return longestSet;
     }
 
-    private const string? ExampleInput = 
+    private const string? ExampleInput =
         """
         kh-tc
         qp-kh
@@ -132,7 +163,7 @@ public class Day23(string? input = null, ITestOutputHelper? outputHelper = null)
         var actual = new Day23(ExampleInput, Output).SolvePartOne();
         Assert.Equal("7", actual);
     }
-    
+
     [Fact]
     public void SolvesPartTwoExample() {
         var actual = new Day23(ExampleInput, Output).SolvePartTwo();
